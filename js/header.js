18ec1635a31bf03e78dffcbde230da3ff945226a
leon773 <64082566+leon773@users.noreply.github.com>
2023-12-11 11:35:27 -0300

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,6 +1,6 @@
 document.getElementById("header").innerHTML= `
 <nav class="menu">
             <a href="https://leon773.github.io/grupo4codo/">Recetario</a>
-            <a href="../templates/ingresar_producto.html">Registrar Recetas</a>
+            <a href="ingresar_producto.html">Registrar Recetas</a>
 </nav>
 `
